334706951b277160204b23e7b0f3ba99cf7403f2
Valerio Billi <valerio.billi18@gmail.com>
2023-05-23 11:00:46 +0200

    add new content

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 //Importiamo i componenti da usare
 import AppTitle from './components/AppTitle.vue'
 import AppContent from './components/AppContent.vue'
+import AppNewContent from './components/AppNewContent.vue'
 
 //Definiamo l'oggetto che conterrà la lista dei componenti importati, che vogliamo usare
 export default {
   components: {
     AppTitle,
-    AppContent
+    AppContent,
+    AppNewContent
   }
 }
 </script>
@@ -25,6 +27,9 @@ export default {
     <!-- Usiamo i componenti importati sopra -->
     <!-- il contenuto è nei relativi file (cartella "components") -->
     <AppContent />
+
+    <AppNewContent />
+
   </main>
 </template>
 
---
+++ src/components/AppNewContent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppNewContent"
+}
+</script>
+
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur incidunt, autem nemo porro nisi
+            reprehenderit iure optio sint, velit architecto odit corporis deleniti mollitia voluptates fuga tempore nulla
+            molestias eaque.</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    color: darkgreen;
+    text-decoration: underline;
+}
+</style>
